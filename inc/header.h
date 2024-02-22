c92291472671a47ac9cd3f1ff498744bf7e3b2ed
ydmytruk <ydmytruk@stud.khpi.ucode-connect.study>
2024-02-22 10:34:59 +0200

    update

--- inc/header.h
+++ inc/header.h
@@ -11,5 +11,13 @@ void mx_printstr(const char *s);
 void mx_printchar(char c);
 void mx_printerr(const char *s);
 
+//mx_file_to_str
+char *mx_strjoin(char const *s1, char const *s2);
+char *mx_strcpy(char *dst, const char *src);
+char *mx_file_to_str(const char *filename);
+char *mx_strcat(char *s1, const char *s2);
+char *mx_strdup(const char *str);
+char *mx_strnew(const int size);
+int mx_strlen(const char *s);
 
 #endif
