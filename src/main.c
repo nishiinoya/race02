a7b655196a83924c969f0ab50a780f53f9e8002d
nishiinoya <omalanchuk.wrk@gmail.com>
2024-02-22 16:01:10 +0100

    map func

--- src/main.c
+++ src/main.c
@@ -15,15 +15,23 @@ int main(int argc, char *argv[]) {
     // err : mx_printerr("route not found");
     // err out of range mx_printerr("points are out of map range");
 
-    return 0;
+   
 
     int file = open(argv[1], O_RDONLY);
     if(file < 0) {
         mx_printerr("map does not exist\n");
         exit(0);
     }
-    if(close(file) < 0) {
+    int x1 = mx_atoi(argv[2]), x2 = mx_atoi(argv[4]);
+    int y1 = mx_atoi(argv[3]), y2 = mx_atoi(argv[5]);
+
+    char *str_file = mx_file_to_str(argv[1]);
+
+    // we need to add a func that open a file and checks a chars inside
+
+    if (close(file) < 0) {
         mx_printerr("error\n");
         exit(0);
     }
+    return 0;
 }
