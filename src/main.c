eaae79ce45b45f3f5d603608a23073fc4046c505
nishiinoya <omalanchuk.wrk@gmail.com>
2024-02-22 18:53:09 +0100

    update: added error handling

--- src/main.c
+++ src/main.c
@@ -5,25 +5,12 @@ int main(int argc, char *argv[]) {
         mx_printerr("usage: ./way_home [file_name] [x1] [y1] [x2] [y2]\n");
         exit(0);
     }
-
     int x1 = mx_atoi(argv[2]);
-	int y1 = mx_atoi(argv[3]);
-	int x2 = mx_atoi(argv[4]);
-	int y2 = mx_atoi(argv[5]);
+    int y1 = mx_atoi(argv[3]);
+    int x2 = mx_atoi(argv[4]);
+    int y2 = mx_atoi(argv[5]);
+    Size map_size = read_map(argv[1]);
+    error_map_check(argv, x1, y1, x2, y2, map_size);
     
-    int map = open(argv[1], O_RDONLY);
-    if (map < 0) {
-        mx_printerr("map does not exist\n");
-        exit(0);
-    }
-
-
-    char *str_file = mx_file_to_str(argv[1]);
-
-    // we need to add a func that open a file and checks a chars inside
-    if (close(map) < 0) {
-        mx_printerr("error\n");
-        exit(0);
-    }
     return 0;
 }
--- src/mx_file_to_str.c
+++ src/mx_file_to_str.c
@@ -1,31 +1,40 @@
 #include "../inc/header.h"
 
-char *mx_file_to_str(const char *filename) 
-{
+//modified to skip comas
+char *mx_file_to_str(const char *filename, char skip_symbol) {
     int size = 0;
     char c;
 
     if (filename == NULL)
-         return NULL;
-    
+        return NULL;
+
     int fileToOpen = open(filename, O_RDONLY);
     if (fileToOpen < 0)
-         return NULL;
+        return NULL;
 
+    // Calculate the size excluding the skipped symbol
     while (read(fileToOpen, &c, 1))
-        size++;
-    
+        if (c != skip_symbol){
+            size++;
+        }
+
     close(fileToOpen);
-    
+
     fileToOpen = open(filename, O_RDONLY);
     if (fileToOpen < 0)
-         return NULL;
-    
-    char *str = mx_strnew(size);
+        return NULL;
 
-    for (int i = 0; read(fileToOpen, &c, 1); i++) 
-        str[i] = c;
-    
+    char *str = mx_strnew(size);
+    int index = 0;
+
+    // Read characters and skip the specified symbol
+    while (read(fileToOpen, &c, 1)) {
+        if (c != skip_symbol) {
+            str[index] = c;
+            index++;
+        }
+    }
+
     close(fileToOpen);
     return str;
 }
