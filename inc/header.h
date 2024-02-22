6d12d70fe9be4a19af067bdffc0ca8507fa817ba
nishiinoya <omalanchuk.wrk@gmail.com>
2024-02-22 12:31:04 +0100

    header + atoi

--- inc/header.h
+++ inc/header.h
@@ -11,8 +11,6 @@ void mx_printstr(const char *s);
 void mx_printchar(char c);
 void mx_printerr(const char *s);
 
-<<<<<<< HEAD
-=======
 //mx_file_to_str
 char *mx_strjoin(char const *s1, char const *s2);
 char *mx_strcpy(char *dst, const char *src);
@@ -21,6 +19,7 @@ char *mx_strcat(char *s1, const char *s2);
 char *mx_strdup(const char *str);
 char *mx_strnew(const int size);
 int mx_strlen(const char *s);
+//atoi
+int mx_atoi(const char *str);
 
->>>>>>> 6b41871bf41ea77ceb512cc796e380eb539c5586
 #endif
